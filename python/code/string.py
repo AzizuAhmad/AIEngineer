7226abd2077f0a8e195904517041abbfb0b44aee
Azizu Ahmad <azizu.rozaki@gmail.com>
2024-09-01 15:15:38 +0700

    python code

---
+++ python/code/string.py
@@ -0,0 +1,16 @@
+text = "ice cream"
+print(text)
+
+print(text[0])
+print(text[0:3])
+print(text[:4])
+print(text[4:])
+
+testing = '''fdsafsafdasf
+dsfafdsafasdf
+dfsafasdf'''
+print(testing)
+
+a = "aku"
+b = "anak"
+print(a+b)
--- python/code/variable.py
+++ python/code/variable.py
@@ -1,1 +1,7 @@
 print("gello")
+rent =  4
+gas = 220
+gro = 30.2
+
+total = rent+gas+gro
+print(total)
